d97ccc33c165e77e0380855049278bf040be704a
Evgenii Mizulin <evgenii_mizulin@epam.com>
2017-10-12 11:24:50 +0300

    2.0.1

--- index.js
+++ index.js
@@ -1,45 +1,41 @@
-const tv4 = require('tv4')
+'use strict';
 
-const CODES = Object.keys(tv4.errorCodes).reduce((res, item) => {
-  const key = tv4.errorCodes[item] * 1
-  res[key] = item
-  return res
-}, {})
+var tv4 = require('tv4');
 
-module.exports = function(opt) {
-  const option = opt || {}
-  const defaultErrorMessage = option.defaultErrorMessage || 'Technical error'
-  const showErrorCode = option.showErrorCode === false ? false : true
+var CODES = Object.keys(tv4.errorCodes).reduce(function (res, item) {
+  var key = tv4.errorCodes[item] * 1;
+  res[key] = item;
+  return res;
+}, {});
 
-  return (value, schema, messages) => {
-    const validation = tv4.validateMultiple(value, schema)
-    const valid = validation.valid
-    const errors = validation.errors.map(item => {
-      const message = defaultErrorMessage + (showErrorCode ? ' (code: ' + CODES[item.code] + ')' : '')
-      const dataPath = item.dataPath
-        ? item.dataPath
-            .slice(1)
-            .split('/')
-            .filter(i => i)
-        : []
-      const schemaPath = item.schemaPath
-        ? item.schemaPath
-            .slice(1)
-            .split('/')
-            .filter(i => i)
-        : []
+module.exports = function (opt) {
+  var option = opt || {};
+  var defaultErrorMessage = option.defaultErrorMessage || 'Technical error';
+  var showErrorCode = option.showErrorCode === false ? false : true;
+
+  return function (value, schema, messages) {
+    var validation = tv4.validateMultiple(value, schema);
+    var valid = validation.valid;
+    var errors = validation.errors.map(function (item) {
+      var message = defaultErrorMessage + (showErrorCode ? ' (code: ' + CODES[item.code] + ')' : '');
+      var dataPath = item.dataPath ? item.dataPath.slice(1).split('/').filter(function (i) {
+        return i;
+      }) : [];
+      var schemaPath = item.schemaPath ? item.schemaPath.slice(1).split('/').filter(function (i) {
+        return i;
+      }) : [];
       return {
         params: item.params,
         code: CODES[item.code],
         dataPath: dataPath,
         schemaPath: schemaPath,
-        message: schemaPath.reduce((res, item) => {
-          if (res === message) return message
-          return res[item] || message
+        message: schemaPath.reduce(function (res, item) {
+          if (res === message) return message;
+          return res[item] || message;
         }, messages || {})
-      }
-    })
+      };
+    });
 
-    return { valid, errors }
-  }
-}
+    return { valid: valid, errors: errors };
+  };
+};
